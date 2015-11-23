0bc9b0738c909bd6dbb785c1f3a374d80e65d7b5
ChrisYuu <nihaoyff@163.com>
2015-11-23 15:00:59 +0800

    撤销后重新提交

--- myapplication2/src/main/java/com/example/chris/myapplication/Teacher.java
+++ myapplication2/src/main/java/com/example/chris/myapplication/Teacher.java
@@ -3,7 +3,5 @@ package com.example.chris.myapplication;
 /**
  * Created by Chris on 2015/11/23.
  */
-public class Test {
-    private String name;
-    private String age;
+public class Teacher {
 }
